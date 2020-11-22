0b162ac405fb9c5a41193e4e1ad9df7e7b18a788
Stephen Oliver <steve@infincia.com>
2020-11-22 15:40:19 -0500

    Explicitly depend on libusb1.0

--- install_dep.sh
+++ install_dep.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-apt -y install build-essential autotools-dev automake libtool autoconf librtlsdr-dev libncurses5-dev || exit 1
+apt -y install build-essential autotools-dev automake libtool autoconf libusb-1.0-0-dev librtlsdr-dev libncurses5-dev || exit 1
--- package.sh
+++ package.sh
@@ -38,6 +38,7 @@ rm ${PACKAGE_NAME}_${VERSION//v}_${PACKAGE_ARCH}.deb > /dev/null 2>&1
 fpm -a ${PACKAGE_ARCH} -s dir -t deb -n ${PACKAGE_NAME} -v ${VERSION//v} -C ${TMPDIR} \
   --after-install after-install.sh \
   -d "librtlsdr-dev" \
+  -d "libusb-1.0-0-dev" \
   -d "libncurses5" \
   -p ${PACKAGE_NAME}_VERSION_ARCH.deb || exit 1
 
