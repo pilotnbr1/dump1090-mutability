aea8e0cab018d2199fb2472cb808deb82528bad3
Stephen Oliver <steve@infincia.com>
2020-12-06 11:05:19 -0500

    Switch to 2.1 repo

--- package.sh
+++ package.sh
@@ -49,9 +49,9 @@ fpm -a ${PACKAGE_ARCH} -s dir -t deb -n ${PACKAGE_NAME} -v ${VERSION//v} -C ${TM
 git describe --exact-match HEAD > /dev/null 2>&1
 if [[ $? -eq 0 ]]; then
     echo "Pushing package to OpenHD repository"
-    cloudsmith push deb openhd/openhd-2-0/${OS}/${DISTRO} ${PACKAGE_NAME}_${VERSION//v}_${PACKAGE_ARCH}.deb || exit 1
+    cloudsmith push deb openhd/openhd-2-1/${OS}/${DISTRO} ${PACKAGE_NAME}_${VERSION//v}_${PACKAGE_ARCH}.deb || exit 1
 else
     echo "Pushing package to OpenHD testing repository"
-    cloudsmith push deb openhd/openhd-2-0-testing/${OS}/${DISTRO} ${PACKAGE_NAME}_${VERSION//v}_${PACKAGE_ARCH}.deb || exit 1
+    cloudsmith push deb openhd/openhd-2-1-testing/${OS}/${DISTRO} ${PACKAGE_NAME}_${VERSION//v}_${PACKAGE_ARCH}.deb || exit 1
 fi
 
